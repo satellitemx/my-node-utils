9a33d43363c2b6c11a9d7d79051566942a526ee1
Siyuan You <satellitem@me.com>
2020-11-01 22:07:54 +1100

    edid parser, finished bytes 0-24

--- README.md
+++ README.md
@@ -15,3 +15,24 @@ A script which prints some header values of given BMP file.
 
 ### Usage
 `node bmp-reader.js <input.bmp>`
+
+## EDID Parser (WIP)
+
+A script which parses supplied EDID. 
+
+### Usage
+`node edid-parser.js <EDID>`
+
+### How to get EDID
+
+#### Mac
+
+Execute `ioreg -lw0 | grep IODisplayEDID` to get EDIDs for all connected monitors. 
+
+#### Linux
+
+Execute `sudo find /sys | grep -i edid` to get EDID files of all connected monitors. 
+
+#### Windows
+
+Please consult the utility softwares that comes with your graphics card driver. 
---
+++ edid-parser.js
@@ -0,0 +1,125 @@
+// Check if edid supplied in commandline
+if (process.argv.length === 3) {
+    EDID = Buffer.from(process.argv[2], "hex")
+} else {
+    console.log("Usage: node edid-parser.js <EDID>")
+    process.exit(1)
+}
+
+// Validate EDID
+const validateEDID = data => {
+    const sum = EDID.reduce((cur, sum) => sum += cur, 0)
+    if (sum % 256 !== 0) {
+        console.log("Invalid EDID. ")
+        process.exit(1)
+    }
+}
+validateEDID()
+
+const Information = {
+    digital: null
+}
+
+// EDID 1.4 format from https://en.wikipedia.org/wiki/Extended_Display_Identification_Data
+
+const getManDate = data => {
+    return `Week ${data[0]}, ${1990 + data[1]}`
+}
+
+const getEDIDVersion = data => {
+    switch (data) {
+        case 1: return "1.3/1.4"
+        default: return "Unknown"
+    }
+}
+
+const getEDIDRevision = data => {
+    switch (data) {
+        case 3: return "1.3"
+        case 4: return "1.4"
+        default: return "Unknown"
+    }
+}
+
+const getVideoInput = data => {
+    const bits = data.toString(2)
+    if (bits[0] === 0) {
+        Information.digital = false
+        return "Analogue, detail not available yet. "
+    }
+    Information.digital = true
+    const bitDepth = getBitDepth(bits.slice(1, 4))
+    const input = getInputInterface(bits.slice(4, 8))
+
+    return `${input}\nBit Depth\t\t${bitDepth}`
+}
+
+const getBitDepth = bits => {
+    switch (bits) {
+        case "000": return "Undefined"
+        case "001": return "6-bit"
+        case "010": return "8-bit"
+        case "011": return "10-bit"
+        case "100": return "12-bit"
+        case "101": return "14-bit"
+        case "110": return "16-bit"
+        case "111": return "Reserved"
+        default: return "Unknown"
+    }
+}
+
+const getInputInterface = bits => {
+    switch (bits) {
+        case "0000": return "Undefined"
+        case "0010": return "HDMIa"
+        case "0011": return "HDMIb"
+        case "0100": return "MDDI"
+        case "0101": return "DisplayPort"
+        default: return "Unknown"
+    }
+}
+
+const calcGamma = data => {
+    return (data + 100) / 100
+}
+
+const getSupportedFeatures = data => {
+    const bits = data.toString(2)
+
+    return `
+    DPMS Standby: \t${bits[0] === 1}
+    DPMS Suspend: \t${bits[1] === 1}
+    DPMS Active-off: \t${bits[2] === 1}
+    Display Type: \t${getDisplayType(bits.slice(3, 5))}
+    sRGB Colour Space: \t${bits[5] === 1}`
+}
+
+const getDisplayType = bits => {
+    if (Information.digital) {
+        switch (bits) {
+            case "00": return "RGB 4:4:4"
+            case "01": return "RGB 4:4:4 + YCrCb 4:4:4"
+            case "10": return "RGB 4:4:4 + YCrCb 4:2:2"
+            case "11": return "RGB 4:4:4 + YCrCb 4:4:4 + YCrCb 4:2:2"
+        }
+    } else {
+        switch (bits) {
+            case "00": return "monochrome or grayscale"
+            case "01": return "RGB colour"
+            case "10": return "non-RGB colour"
+            case "11": return "Undefined"
+        }
+    }
+}
+
+console.log(`
+Vendor ID: \t\t${EDID.slice(8, 10).toString("hex")}
+Product ID: \t\t${EDID.slice(10, 12).reverse().toString("hex")}
+Manufacture Date: \t${getManDate(EDID.slice(16, 18))}
+EDID Version: \t\t${getEDIDVersion(EDID[18])}
+EDID Revision: \t\t${getEDIDRevision(EDID[19])}
+Video Interface: \t${getVideoInput(EDID[20])}
+Screen Size: \t\t${EDID[21]}✕${EDID[22]} cm
+Gamma (Computer): \t${calcGamma(EDID[23])}
+Supported Features: ${getSupportedFeatures(EDID[24])}
+`)
